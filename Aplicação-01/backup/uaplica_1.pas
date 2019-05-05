490d3302d9b438858486ede8da594004f1462ca0
JacobBlackzx <ti.vicktorzx@gmail.com>
2019-05-05 19:55:59 -0300

    Aplicação - 1

---
+++ Aplicação-01/backup/uaplica_1.pas
@@ -0,0 +1,59 @@
+unit uAplica_1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btnLimpar: TButton;
+    btnInserir: TButton;
+    txtCaixa: TEdit;
+    Label1: TLabel;
+    procedure btnInserirClick(Sender: TObject);
+    procedure btnLimparClick(Sender: TObject);
+    procedure txtCaixaChange(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Label1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.txtCaixaChange(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.btnLimparClick(Sender: TObject);
+begin
+  txtCaixa.text:='Limpar Caixa de Texto';
+end;
+
+procedure TForm1.btnInserirClick(Sender: TObject);
+begin
+  txtCaixa.text:='Inserir Dados na Caixa de Texto';
+end;
+
+end.
+

e932b9fba98e2051b835915380869bfa956d8433
Vitor Hugo <ti.vicktorzx@gmail.com>
2022-11-30 22:51:02 -0300

    Criação de construtores e melhorias na interface

---
+++ fluxocaixa/view/backup/uconfigurabanco.pas
@@ -0,0 +1,105 @@
+unit uconfigurabanco;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, IniFiles;
+
+type
+
+  { Tfrmconfigurabanco }
+
+  Tfrmconfigurabanco = class(TForm)
+    btnSalvar: TButton;
+    btnCancelar: TButton;
+    edtBanco: TEdit;
+    edtSenha: TEdit;
+    edtUsuario: TEdit;
+    edtServer: TEdit;
+    edtPorta: TEdit;
+    lblSenha: TLabel;
+    lblUsuario: TLabel;
+    lblServidor: TLabel;
+    lblBanco: TLabel;
+    lblConfiguraBanco: TLabel;
+    lblPorta: TLabel;
+    shaLinhaCB: TShape;
+    procedure btnCancelarClick(Sender: TObject);
+    procedure btnSalvarClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+     procedure salva_ini;
+     procedure ler_ini;
+  public
+
+  end;
+
+var
+  frmconfigurabanco: Tfrmconfigurabanco;
+
+implementation
+ uses uprincipal;
+
+{$R *.lfm}
+
+{ Tfrmconfigurabanco }
+
+procedure Tfrmconfigurabanco.btnSalvarClick(Sender: TObject);
+begin
+  salva_ini;
+  Close;
+end;
+
+procedure Tfrmconfigurabanco.btnCancelarClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure Tfrmconfigurabanco.FormShow(Sender: TObject);
+begin
+  ler_ini;
+end;
+
+procedure Tfrmconfigurabanco.salva_ini;
+// Criar Variavel
+var
+  ArqINI: TIniFile;
+begin
+  ArqINI := TIniFile.Create(cfg_arqINI);
+  try
+    ArqINI.WriteString('ConexaoDB','Banco',edtBanco.Text);
+    ArqINI.WriteString('ConexaoDB','Servidor',edtServer.Text);
+    ArqINI.WriteInteger('ConexaoDB','Porta',StrToIntDef(edtPorta.Text,3306));
+    ArqINI.WriteString('ConexaoDB','Usuario',edtUsuario.Text);
+    ArqINI.WriteString('ConexaoDB','Senha',edtSenha.Text);
+  finally
+    ArqINI.Free;
+  end;
+end;
+
+procedure Tfrmconfigurabanco.ler_ini;
+var
+  ArqINI: TIniFile;
+begin
+  ArqINI := TIniFile.Create(cfg_arqINI);
+  try
+    edtBanco.Text := ArqINI.ReadString('ConexaoDB','Banco','');
+    edtServer.Text := ArqINI.ReadString('ConexaoDB','Servidor','');
+    edtPorta.Text := IntToStr(ArqINI.ReadInteger('ConexaoDB','Porta',3306));
+    edtUsuario.Text := ArqINI.ReadString('ConexaoDB','Usuario','');
+    edtSenha.Text := ArqINI.ReadString('ConexaoDB','Senha','');
+  finally
+    ArqINI.Free;
+  end;
+end;
+
+//
+
+
+
+
+
+end.
+
--- fluxocaixa/view/uprincipal.pas
+++ fluxocaixa/view/uprincipal.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  Buttons;
+  Buttons, uconfigurabanco;
 
 type
 
@@ -14,16 +14,19 @@ type
 
   Tfrmprincipal = class(TForm)
     lblFluxoCaixa: TLabel;
-    pnpEsquerda: TPanel;
-    shaLinha: TShape;
+    pnpEsqueda: TPanel;
+    shaLinhaPrincipal: TShape;
     btnContas: TSpeedButton;
     btnPlanos: TSpeedButton;
-    btnLancamento: TSpeedButton;
+    btnLancamentos: TSpeedButton;
     btnSair: TSpeedButton;
     btnConfigura: TSpeedButton;
+    procedure btnConfiguraClick(Sender: TObject);
     procedure btnSairClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
+    procedure ler_ini;
 
   public
 
@@ -31,6 +34,7 @@ type
 
 var
   frmprincipal: Tfrmprincipal;
+  cfg_arqINI, cfg_pathApp: String;
 
 implementation
 
@@ -40,16 +44,25 @@ implementation
 
 procedure Tfrmprincipal.btnSairClick(Sender: TObject);
 begin
-// Construtor para habilitar evento sair do botão Sair da Tela Principal
+// Construtor sair do botão Tela Principal
   ShowMessage('Até Breve !!!');
   Application.Terminate;
 end;
 
+procedure Tfrmprincipal.btnConfiguraClick(Sender: TObject);
+begin
+  frmconfigurabanco := Tfrmconfigurabanco.Create(Self);
+  try
+    frmconfigurabanco.ShowModal;
+  finally
+    FreeAndNil(frmconfigurabanco);
+  end;
+end;
+
 procedure Tfrmprincipal.FormCreate(Sender: TObject);
 begin
-  // Formatação de Compatibilidade Microsoft e Linux
+// Formatação de Compatibilidade Microsoft e Linux
   {$IFDEF LINUX}
-
   // Formatação de Moedas
   CurrencyString    := 'R$';
   CurrencyFormat    := 2;
@@ -59,9 +72,44 @@ begin
   // Formatação de Datas
   DateSeparator     := '/';
   ShortDateFormat   := 'dd/mm/yyyy';
-
   {$ENDIF}
+
+// Estrutura CFG
+  cfg_arqINI       := ChangeFileExt(ParamStr(0),'ini');
+  //ShowMessage(cfg_arqINI);
+
 end;
+
+procedure Tfrmprincipal.FormShow(Sender: TObject);
+begin
+  if not FileExists(cfg_arqINI) then
+     btnConfigura.Click;
+
+end;
+
+  procedure Tfrmprincipal.ler_ini;
+var
+  ArqINI: TIniFile;
+begin
+  ArqINI := TIniFile.Create(cfg_arqINI);
+  try
+    edtBanco.Text := ArqINI.ReadString('ConexaoDB','Banco','');
+    edtServer.Text := ArqINI.ReadString('ConexaoDB','Servidor','');
+    edtPorta.Text := IntToStr(ArqINI.ReadInteger('ConexaoDB','Porta',3306));
+    edtUsuario.Text := ArqINI.ReadString('ConexaoDB','Usuario','');
+    edtSenha.Text := ArqINI.ReadString('ConexaoDB','Senha','');
+  finally
+    ArqINI.Free;
+  end;
+
+end;
+
+
+
+
+
+
+
 
 end.
 
